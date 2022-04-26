efd071fae1d521e9fb6850c35d65ee0f7ff76397
Adriano Amaricci <adriano.amaricci@gmail.com>
2022-04-26 15:17:26 +0200

    Intermediate commit.
    1d_Kondo get optical trap potential
    Vtrap = Potential amplitude
    Atrap = Potential level

--- 1d_Kondo_chain.f90
+++ 1d_Kondo_chain.f90
@@ -1,3 +1,10 @@
+!       \                 /
+!          \           / 
+!             \  _  / 
+!       1--2--3--4--5--6--7
+!                o
+!                I 
+!
 program Kondo1d
   USE EDLAT
   USE SCIFOR
@@ -6,7 +13,7 @@ program Kondo1d
 #endif
   implicit none
   character(len=16)   :: finput
-  real(8)             :: ts,gamma
+  real(8)             :: ts,vtrap,atrap
   integer             :: N,N1,i
   logical             :: pbc
   !
@@ -16,20 +23,22 @@ program Kondo1d
   !
   call parse_cmd_variable(finput,"FINPUT",default='inputED.conf')
   call parse_input_variable(ts,"TS",finput,default=0.25d0,comment="chain hopping parameter")
+  call parse_input_variable(atrap,"atrap",finput,default=0d0,comment="optical trap bottom energy ")
+  call parse_input_variable(vtrap,"vtrap",finput,default=0d0,comment="optical trap potential amplitude: 1/2.V.x**2")
   call parse_input_variable(pbc,"PBC",finput,default=.false.,comment="T: PBC, F: OBC")
-  call parse_input_variable(gamma,"GAMMA",finput,default=0.01d0,comment="field amplitude")
   call ed_read_input(trim(finput))
   !
 
   if(any(Nsites(1:Norb)==0))stop "This driver is for Kondo problem only: Nsites=[1,N]"
 
+
   call ed_Hij_init(Nsites(1:Norb))
 
   N  = Nsites(1)                !odd
   N1 = (N+1)/2                  !N1%2==0
-  !                I 
-  !                o
-  !       1--2--3--4--5--6--7--8
+
+
+
   call ed_Hij_add_link(1,2,1,1,1,one*ts)
   do i=2,N-1
      call ed_Hij_add_link(i,i-1,1,1,1,one*ts)
@@ -41,6 +50,11 @@ program Kondo1d
      call ed_Hij_add_link(1,N,1,1,1,one*ts)
      call ed_Hij_add_link(N,1,1,1,1,one*ts)
   end if
+  do i=1,N
+     write(100,*)i,0,e_local(i)
+     call ed_Hij_add_link(i,i,1,1,1,one*e_local(i))
+  enddo
+
   !
   call ed_Hij_info()
   Jkindx = N1
@@ -54,6 +68,14 @@ program Kondo1d
   call finalize_MPI()
 #endif
 
+contains
+  function e_local(i) result(e)
+    integer :: i
+    real(8) :: e
+    real(8) :: r2
+    r2 = dble(i-N1)**2
+    e  = atrap + 0.5d0*Vtrap*r2
+  end function e_local
 
 end program Kondo1d
 
