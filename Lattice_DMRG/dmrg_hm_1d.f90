e2732222ecb320c51d66ee7607805e185db510d2
Adriano Amaricci <adriano.amaricci@gmail.com>
2025-10-08 15:02:20 +0200

    Updated DMRG codes with latest development in Lattice_DMRG

--- Lattice_DMRG/dmrg_hm_1d.f90
+++ Lattice_DMRG/dmrg_hm_1d.f90
@@ -10,12 +10,12 @@ program hubbard_1d
   character(len=64)                              :: finput
   integer                                        :: i,unit,iorb,ispin
   real(8)                                        :: ts(2),Mh(2),lambda
-  type(site),dimension(:),allocatable            :: Dot
+  type(site),dimension(:),allocatable            :: MyDot
   type(sparse_matrix),dimension(:,:),allocatable :: N,C
   type(sparse_matrix),dimension(:),allocatable   :: dens,docc,sz,s2z,Mvec
   real(8),dimension(:,:),allocatable             :: Hloc,Hlr
   integer                                        :: irank,comm,rank,ierr
-  logical                                        :: master
+  logical                                        :: master,imeasure
 
 #ifdef _MPI  
   call init_MPI()
@@ -33,7 +33,9 @@ program hubbard_1d
   call parse_input_variable(Mh,"MH",finput,default=(/(0d0,i=1,2 )/),&
        comment="Crystal field splittings")
   call parse_input_variable(lambda,"LAMBDA",finput,default=0d0,&
-       comment="off-diagonal amplitude")  
+       comment="off-diagonal amplitude")
+  call parse_input_variable(imeasure,"imeasure",finput,default=.true.,&
+       comment="Bool to include measurements directly in the run.")
   call read_input(finput)
 
 
@@ -42,8 +44,8 @@ program hubbard_1d
 
   Hloc = diag([Mh(1:Norb),Mh(1:Norb)])
 
-  allocate(Dot(1))
-  Dot = electron_site()
+  allocate(MyDot(1))
+  MyDot = electron_site()
 
 
   if(allocated(Hlr))deallocate(Hlr)
@@ -52,33 +54,33 @@ program hubbard_1d
   if(Norb==2)Hlr = Hlr + lambda*kron(pauli_0,pauli_x)
 
 
-  call init_dmrg(Hlr,ModelDot=Dot)
+  call init_dmrg(Hlr,ModelDot=MyDot)
 
 
   !Run DMRG algorithm
   call run_DMRG()
 
 
-  !Post-processing and measure quantities:
-  !Measure <Sz(i)>
-  allocate(C(Norb,Nspin),N(Norb,Nspin))
-  do ispin=1,Nspin
-     do iorb=1,Norb
-        C(iorb,ispin) = dot(1)%operators%op(key="C"//dot(1)%okey(iorb,ispin))
-        N(iorb,ispin) = matmul(C(iorb,ispin)%dgr(),C(iorb,ispin))
+  if(imeasure)then
+     !Post-processing and measure quantities:
+     !Measure <Sz(i)>
+     allocate(C(Norb,Nspin),N(Norb,Nspin))
+     do ispin=1,Nspin
+        do iorb=1,Norb
+           C(iorb,ispin) = myDot(1)%operators%op(key="C"//myDot(1)%okey(iorb,ispin))
+           N(iorb,ispin) = matmul(C(iorb,ispin)%dgr(),C(iorb,ispin))
+        enddo
      enddo
-  enddo
-  allocate(Mvec(3*Norb),sz(Norb))
-  do iorb=1,Norb
-     sz(iorb)          = n(iorb,1)-n(iorb,2)
-     Mvec(iorb)        = n(iorb,1)+n(iorb,2)
-     Mvec(iorb+Norb)   = matmul(n(iorb,1),n(iorb,2))
-     Mvec(iorb+2*Norb) = matmul(sz(iorb),sz(iorb))
-  enddo
-
-
-  call Measure_DMRG(Mvec,file="n_d_s2z_l1VSj", pos=arange(1,Ldmrg))
-
+     allocate(Mvec(3*Norb),sz(Norb))
+     do iorb=1,Norb
+        sz(iorb)          = n(iorb,1)-n(iorb,2)
+        Mvec(iorb)        = n(iorb,1)+n(iorb,2)
+        Mvec(iorb+Norb)   = matmul(n(iorb,1),n(iorb,2))
+        Mvec(iorb+2*Norb) = matmul(sz(iorb),sz(iorb))
+     enddo
+     !
+     call Measure_DMRG(Mvec,file="n_d_s2z_l1VSj", pos=arange(1,Ldmrg))
+  endif
 
 
   !Finalize DMRG
--- Lattice_DMRG/dmrg_spin_1d.f90
+++ Lattice_DMRG/dmrg_spin_1d.f90
@@ -12,7 +12,7 @@ program dmrg_spin_1d
   type(sparse_matrix)                 :: bSz,bSp,SiSj
   real(8),dimension(:,:),allocatable  :: Hlr
   integer                             :: irank,comm,rank,ierr
-  logical                             :: master
+  logical                             :: master,imeasure
 
 #ifdef _MPI  
   call init_MPI()
@@ -21,7 +21,7 @@ program dmrg_spin_1d
   rank = get_Rank_MPI(comm)
   master = get_Master_MPI(comm)
 #endif
-  
+
   call parse_cmd_variable(finput,"FINPUT",default='DMRG.conf')
   call parse_input_variable(SUN,"SUN",finput,default=2,&
        comment="Spin SU(N) value. 2=> spin 1/2, 3=> spin 1")
@@ -29,6 +29,8 @@ program dmrg_spin_1d
        comment="Magnetic field noise amplitude")
   call parse_input_variable(Hvec,"Hvec",finput,default=[0d0,0d0,0d0],&
        comment="Magnetic field direction")
+  call parse_input_variable(imeasure,"imeasure",finput,default=.true.,&
+       comment="Bool to include measurements directly in the run.")
   call read_input(finput)
 
 
@@ -53,35 +55,36 @@ program dmrg_spin_1d
   call run_DMRG()
 
 
-  !Post-processing and measure quantities:
-  !Measure <Sz(i)>
-  call Measure_DMRG(MyDot(1)%operators%op(key="S_z"),file="SzVSj")
-
-  
 
 
-  !Measure <S(i).S(i+1)>
-  if(master)unit=fopen("SiSjVSsite"//str(label_DMRG('u')),append=.true.)
-  call Init_measure_dmrg("SiSjVSsite")
-  do pos=1,Ldmrg-1
-     bSz = Build_Op_DMRG(MyDot(1)%operators%op("S_z"),pos,set_basis=.true.)
-     bSp = Build_Op_DMRG(MyDot(1)%operators%op("S_p"),pos,set_basis=.true.)
-     SiSj= get_SiSj(bSz,bSp,MyDot(1)%operators%op("S_z"),MyDot(1)%operators%op("S_p"))
-     SiSj= Advance_Corr_DMRG(SiSj,pos)
-     Sij = Average_Op_DMRG(SiSj,pos)
-     if(master)write(unit,*)pos,Sij
-     if(master)call eta(pos,Ldmrg-1)
-  enddo
-  call End_measure_dmrg()
-  if(Master)close(unit)
+  if(imeasure)then
+     !Post-processing and measure quantities:
+     !Measure <Sz(i)>
+     call Measure_DMRG(MyDot(1)%operators%op(key="S_z"),file="SzVSj")
 
+
+     !Measure <S(i).S(i+1)>
+     if(master)unit=fopen("SiSjVSsite"//str(label_DMRG('u')),append=.true.)
+     call Init_measure_dmrg("SiSjVSsite")
+     do pos=1,Ldmrg-1
+        bSz = Build_Op_DMRG(MyDot(1)%operators%op("S_z"),pos,set_basis=.true.)
+        bSp = Build_Op_DMRG(MyDot(1)%operators%op("S_p"),pos,set_basis=.true.)
+        SiSj= get_SiSj(bSz,bSp,MyDot(1)%operators%op("S_z"),MyDot(1)%operators%op("S_p"))
+        SiSj= Advance_Corr_DMRG(SiSj,pos)
+        Sij = Average_Op_DMRG(SiSj,pos)
+        if(master)write(unit,*)pos,Sij
+        if(master)call eta(pos,Ldmrg-1)
+     enddo
+     call End_measure_dmrg()
+     if(Master)close(unit)
+  endif
 
   !Finalize DMRG
   call finalize_dmrg()
 #ifdef _MPI
   call finalize_MPI()
 #endif
-  
+
 contains
 
 
