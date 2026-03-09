0a371c5744b3b6498da4e9a21c9d4eb5d67d9f8d
Adriano Amaricci <adriano.amaricci@gmail.com>
2026-03-09 11:01:44 +0100

    Add Bethe AFM

--- Dmft_ED/ed_hm_bethe_AFM.f90
+++ Dmft_ED/ed_hm_bethe_AFM.f90
@@ -1,13 +1,13 @@
 program lancED
-  USE DMFT_ED
+  USE EDIPACK
   USE SCIFOR
   USE DMFT_TOOLS
   USE MPI
   implicit none
-  integer                                       :: iloop,Le,Nso,iorb,ispin
+  integer                                       :: iloop,Le,Nso,iorb,ispin,ilat,jorb,jspin,io,jo
   logical                                       :: converged
   real(8),dimension(5)                          :: Wbethe,Dbethe
-  integer                                       :: Nineq,Nlat
+  integer                                       :: Nineq,Nlat,Nlso
   !Bath:
   integer                                       :: Nb
   real(8),allocatable                           :: Bath(:),Bath_prev(:)
@@ -28,9 +28,10 @@ program lancED
   complex(8),allocatable,dimension(:,:,:,:,:,:) :: Gmats
   complex(8),allocatable,dimension(:,:,:,:,:,:) :: Greal
   !
+  complex(8),allocatable,dimension(:,:,:) :: Self
+  !
   integer                                       :: comm,rank
   logical                                       :: master
-  logical                                       :: betheSC,wGimp,mixG0,symOrbs,reGF
 
   call init_MPI()
   comm = MPI_COMM_WORLD
@@ -38,37 +39,30 @@ program lancED
   rank = get_Rank_MPI(comm)
   master = get_Master_MPI(comm)
 
-  Nlat =2                       !two sub-lattices
-  Nineq=1                       !only one is inequivalent (B=-A)
-
-
-  call parse_cmd_variable(finput,"FINPUT",default='inputED.in')
+  call parse_cmd_variable(finput,"FINPUT",default='inputED.conf')
   call parse_input_variable(Le,"LE",finput,default=500)
   call parse_input_variable(Wbethe,"WBETHE",finput,default=[1d0,1d0,1d0,1d0,1d0])
   call parse_input_variable(Dbethe,"DBETHE",finput,default=[0d0,0d0,0d0,0d0,0d0])
   call parse_input_variable(wmixing,"WMIXING",finput,default=0.5d0)
-  call parse_input_variable(betheSC,"BETHESC",finput,default=.false.)
-  call parse_input_variable(wGimp,"wGimp",finput,default=.false.)
-  call parse_input_variable(mixG0,"mixG0",finput,default=.false.)
-  call parse_input_variable(symOrbs,"symOrbs",finput,default=.false.)
-  call parse_input_variable(reGF,"REGF",finput,default=.false.)
   !
-  call ed_read_input(trim(finput),comm)
+  call ed_read_input(trim(finput))
 
   !Add DMFT CTRL Variables:
-  call add_ctrl_var(Nlat,"nlat")
   call add_ctrl_var(Norb,"norb")
   call add_ctrl_var(Nspin,"nspin")
-  call add_ctrl_var(Nineq,"nineq")
   call add_ctrl_var(beta,"beta")
   call add_ctrl_var(xmu,"xmu")
   call add_ctrl_var(wini,'wini')
   call add_ctrl_var(wfin,'wfin')
   call add_ctrl_var(eps,"eps")
 
-  if(Nspin/=2.OR.Norb>5)stop "Wrong setup from input file: Nspin/=2 OR Norb>5"
-  Nso=Nspin*Norb
+  if(Nspin/=2.OR.Norb>1)stop "Wrong setup from input file: Nspin/=2 OR Norb>1"
 
+  Nlat =2                       !two sub-lattices
+  Nineq=1                       !only one is inequivalent (B=-A)
+  Nso=Nspin*Norb
+  Nlso=Nlat*Nso
+  
   allocate(Ebands(Nso,Le))
   allocate(Dbands(Nso,Le))
   allocate(Wband(Nso))
@@ -106,20 +100,21 @@ program lancED
   call ed_init_solver(Bath)
   call ed_break_symmetry_bath(Bath,sb_field,1d0)
 
+  call ed_set_hloc(Hloc)
 
   !DMFT loop
   iloop=0;converged=.false.
   do while(.not.converged.AND.iloop<nloop)
      iloop=iloop+1
-     call start_loop(iloop,nloop,"DMFT-loop")
+     if(master)call start_loop(iloop,nloop,"DMFT-loop")
      !
      !Solve the EFFECTIVE IMPURITY PROBLEM (first w/ a guess for the bath)
      !Solve sublattice A only:
-     call ed_solve(comm,Bath,Hloc)
+     call ed_solve(Bath)
 
      !Retrieve self-energy for A-lattice only:
-     call ed_get_sigma_matsubara(Smats(1,:,:,:,:,:))
-     call ed_get_sigma_realaxis(Sreal(1,:,:,:,:,:))   
+     call ed_get_sigma(Smats(1,:,:,:,:,:),axis='m')
+     call ed_get_sigma(Sreal(1,:,:,:,:,:),axis='r')
      !Impose Neel AFM symmetry: B_sigma = A_{-sigma}
      do ispin=1,2
         Smats(2,ispin,ispin,:,:,:)=Smats(1,3-ispin,3-ispin,:,:,:)
@@ -128,39 +123,50 @@ program lancED
 
      !Evaluate Gloc and update Weiss, all at once here:
      call get_delta_bethe()
-     !print:
-     call dmft_print_gf_matsubara(Gmats,"Gmats",iprint=4)
-     call dmft_print_gf_matsubara(Greal,"Greal",iprint=4)
-     call dmft_print_gf_matsubara(Weiss,"Weiss",iprint=4)
-     !
-     !
-     !
-     if(mixG0)then
-        if(iloop>1)Weiss = wmixing*Weiss + (1.d0-wmixing)*Weiss_prev
-        Weiss_prev=Weiss
-     endif
+     call dmft_write_gf(Gmats,"Gloc",axis='m',iprint=4)
+     call dmft_write_gf(Greal,"Gloc",axis='r',iprint=4)
+     call dmft_write_gf(Weiss,"Weiss",axis='m',iprint=4)
      !
      !Perform the SELF-CONSISTENCY by fitting the new bath, only A sublattice needs to be fitted.
-     if(symOrbs)then
-        call ed_chi2_fitgf(comm,Weiss(1,:,:,:,:,:),bath,ispin=1,iorb=1)
-        call ed_orb_equality_bath(bath,save=.true.)
-     else
-        call ed_chi2_fitgf(comm,Weiss(1,:,:,:,:,:),bath,ispin=1)
-        call ed_chi2_fitgf(comm,Weiss(1,:,:,:,:,:),bath,ispin=2)
-     endif
+     call ed_chi2_fitgf(Weiss(1,:,:,:,:,:),bath,ispin=1)
+     call ed_chi2_fitgf(Weiss(1,:,:,:,:,:),bath,ispin=2)
      !
      !MIXING:
-     if(.not.mixG0)then
-        if(iloop>1)Bath = wmixing*Bath + (1.d0-wmixing)*Bath_prev
-        Bath_prev=Bath
-     endif
+     if(iloop>1)Bath = wmixing*Bath + (1.d0-wmixing)*Bath_prev
+     Bath_prev=Bath
      !
-     converged = check_convergence(Weiss(1,1,1,1,1,:),dmft_error,nsuccess,nloop,reset=.false.)
-     call ed_get_dens(dens)
-     if(nread/=0d0)call ed_search_variable(xmu,sum(dens),converged)
-
-     call end_loop
+     if(master)converged = check_convergence(Weiss(1,1,1,1,1,:),dmft_error,nsuccess,nloop,reset=.false.)
+     call Bcast_MPI(Comm,converged)
+     if(master)call end_loop
   enddo
+  call ed_finalize_solver()
+
+
+  allocate(Self(Nlso,Nlso,Lmats));Self=zero
+  deallocate(Ebands,Dbands,Wband,H0,de)
+  allocate(Ebands(Nlso,Le))
+  allocate(Dbands(Nlso,Le))
+  allocate(Wband(Nlso))
+  allocate(H0(Nlso))
+  allocate(de(Nlso))
+  do concurrent(ilat=1:Nlat,ispin=1:Nspin,jspin=1:Nspin,iorb=1:Norb,jorb=1:Norb)
+     io = iorb + (ispin-1)*Norb + (ilat-1)*Norb*Nspin
+     jo = jorb + (jspin-1)*Norb + (ilat-1)*Norb*Nspin
+     Self(io,jo,:) = Smats(ilat,ispin,jspin,iorb,jorb,:)
+  enddo  
+  do ilat=1,Nlat
+     do ispin=1,Nspin
+        do iorb=1,Norb
+           io = iorb + (ispin-1)*Norb + (ilat-1)*Norb*Nspin           
+           Wband(io) = Wbethe(iorb)         !band width
+           H0(io)    = Dbethe(iorb)         !Local energy
+           Ebands(io,:) = linspace(-Wband(io),Wband(io),Le,mesh=de(io)) !dispersion
+           Dbands(io,:) = dens_bethe(Ebands(io,:),Wband(io))*de(io)     !DOS
+        enddo
+     enddo
+  enddo  
+  !
+  call dmft_kinetic_energy_Bethe(Ebands,Dbands,H0,Self)
 
 
   call finalize_MPI()
@@ -241,6 +247,197 @@ contains
     enddo
   end subroutine get_delta_bethe
 
+
+
+
+
+
+
+  subroutine dmft_kinetic_energy_Bethe(Ebands,Dbands,Hloc,Sigma,Ekin,Eloc)
+    real(8),dimension(:,:),intent(in)                :: Ebands    ![Nlat*Nspin*Norb][Lk]
+    real(8),dimension(:,:),intent(in)                :: Dbands    ![Nlat*Nspin*Norb][Lk]Lk]
+    real(8),dimension(size(Ebands,1)),intent(in)     :: Hloc    ![Nlat*Nspin*Norb]
+    complex(8),dimension(:,:,:)                      :: Sigma   ![Nlat*Nspin*Norb][Nlat*Nspin*Norb][L]
+    real(8),dimension(size(Ebands,1)),optional       :: Ekin,Eloc
+    !
+    integer                                          :: Lk,Nlso,Liw
+    integer                                          :: i,ik,iso
+    !
+    real(8),dimension(size(Ebands,1),size(Ebands,1)) :: Sigma_HF
+    !
+    complex(8)                                       :: Ak,Bk,Ck,Dk
+    complex(8)                                       :: Gk,Tk
+    !
+    complex(8),dimension(:,:),allocatable            :: Ak_,Bk_,Ck_,Dk_
+    complex(8),dimension(:,:),allocatable            :: Gk_,Tk_
+    !
+    real(8),dimension(size(Ebands,1))                :: Tail0,Tail1
+    real(8),dimension(size(Ebands,1))                :: Lail0,Lail1
+    real(8)                                          :: spin_degeneracy
+    !
+    real(8),dimension(size(Ebands,1))                :: H0,Hl
+    real(8),dimension(size(Ebands,1))                :: H0tmp,Hltmp
+    !
+    real(8),dimension(size(Ebands,1))                :: Ekin_,Eloc_
+    !
+    real(8),dimension(:),allocatable                 :: wm
+    logical                                          :: dos_diag !1. T / 2. F
+    integer                                       :: mpi_rank,mpi_size
+    logical                                       :: mpi_master
+    !
+    !
+    !MPI setup:
+    mpi_size  = get_size_MPI()
+    mpi_rank =  get_rank_MPI()
+    mpi_master= get_master_MPI()
+    !
+    !
+    Nlso = size(Ebands,1)
+    Lk   = size(Ebands,2)
+    Liw  = size(Sigma,3)
+    !
+    !Testing:
+    call assert_shape(Sigma,[Nlso,Nlso,Liw],"dmft_kinetic_energy_normal_Nso_dos","Sigma")
+    !
+    !Allocate and setup the Matsubara freq.
+    if(allocated(wm))deallocate(wm);allocate(wm(Liw))
+    wm = pi/beta*dble(2*arange(1,Liw)-1)
+    !
+    !Get HF part of the self-energy
+    Sigma_HF = dreal(Sigma(:,:,Liw))
+    !
+    !
+    if(mpi_master)write(*,"(A)") "Kinetic energy computation"
+    if(mpi_master)call start_timer()
+    H0=0d0
+    Hl=0d0
+    H0tmp= 0d0
+    Hltmp= 0d0
+    !
+    Tail0=0d0
+    Tail1=0d0
+    Lail0=0d0
+    Lail1=0d0
+    !
+    !Get principal part: Tr[ Hk.(Gk-Tk) ]
+    do ik=1,Lk
+       do iso=1,Nlso
+          Ak = Ebands(iso,ik)
+          Bk =-Ebands(iso,ik) - Sigma_HF(iso,iso)
+          do i=1+mpi_rank,Liw,mpi_size
+             Gk = (xi*wm(i)+xmu) - Sigma(iso,iso,i) - Ebands(iso,ik) - Hloc(iso)
+             Gk = 1d0/Gk
+             Tk = 1d0/(xi*wm(i)) - Bk/(xi*wm(i))**2
+             Ck = Ak*(Gk - Tk)
+             Dk = Hloc(iso)*(Gk - Tk)
+             H0tmp(iso) = H0tmp(iso) + Dbands(iso,ik)*Ck
+             Hltmp(iso) = Hltmp(iso) + Dbands(iso,ik)*Dk
+          enddo
+       enddo
+       if(mpi_master)call eta(ik,Lk)
+    enddo
+    call AllReduce_MPI(MPI_COMM_WORLD,H0tmp,H0)
+    call AllReduce_MPI(MPI_COMM_WORLD,Hltmp,Hl)
+    if(mpi_master)call stop_timer()
+    spin_degeneracy=3d0-Nspin     !2 if Nspin=1, 1 if Nspin=2
+    H0=H0/beta*2*spin_degeneracy
+    Hl=Hl/beta*2*spin_degeneracy
+    !
+    !get tail subtracted contribution: Tr[ Hk.Tk ]
+    do ik=1,Lk
+       do iso=1,Nlso
+          Ak = Ebands(iso,ik)
+          Bk =-Ebands(iso,ik) - Sigma_HF(iso,iso)
+          Ck= Ak*Bk
+          Dk= Hloc(iso)*Bk
+          Tail0(iso) = Tail0(iso) + 0.5d0*Dbands(iso,ik)*Ak
+          Tail1(iso) = Tail1(iso) + 0.25d0*Dbands(iso,ik)*Ck
+          Lail0(iso) = Lail0(iso) + 0.5d0*Dbands(iso,ik)*Hloc(iso)
+          Lail1(iso) = Lail1(iso) + 0.25d0*Dbands(iso,ik)*Dk
+       enddo
+    enddo
+    Tail0=Tail0*spin_degeneracy
+    Tail1=Tail1*beta*spin_degeneracy
+    Lail0=Lail0*spin_degeneracy
+    Lail1=Lail1*beta*spin_degeneracy
+    !
+    Ekin_=H0+Tail0+Tail1
+    Eloc_=Hl+Lail0+Lail1
+    !
+    if(mpi_master)call write_kinetic_value(Ekin_,Eloc_,Nlat,Nspin*Norb)
+    if(present(Ekin))Ekin=Ekin_
+    if(present(Eloc))Eloc=Eloc_
+    !
+    deallocate(wm)
+  end subroutine dmft_kinetic_energy_Bethe
+
+
+
+
+  !+-------------------------------------------------------------------+
+  !PURPOSE  : Write energies to file
+  !+-------------------------------------------------------------------+
+  subroutine write_kinetic_value(Ekin,Eloc,Nlat,Nso)
+    real(8),dimension(:)               :: Ekin
+    real(8),dimension(size(Ekin))      :: Eloc
+    real(8),dimension(:,:),allocatable :: Ekin_,Eloc_
+    integer,optional                   :: Nlat,Nso
+    integer                            :: Nlso
+    integer                            :: i,iso,ilat,unit
+    if(.not.present(Nlat))then
+       !
+       Nlso = size(Ekin)
+       !
+       unit = free_unit()
+       open(unit,file="dmft_kinetic_energy.info")
+       write(unit,"(A1,90(A14,1X))")"#",&
+            str(1)//"<K>",str(2)//"<Eloc>",&
+            (str(2+i)//"<K"//str(i)//">",i=1,Nlso),&
+            (str(2+Nlso+i)//"<Eloc"//str(i)//">",i=1,Nlso)
+       close(unit)
+       !
+       unit = free_unit()
+       open(unit,file="dmft_kinetic_energy.dat")
+       write(unit,"(90F15.9)")sum(Ekin),sum(Eloc),(Ekin(i),i=1,Nlso),(Eloc(i),i=1,Nlso)
+       close(unit)
+       !
+    else
+       !
+       if(.not.present(Nso))stop "ERROR write_kinetic_value: Nlat present but Nso not present."
+       !
+       Nlso = size(Ekin)
+       if(Nlso /= Nlat*Nso)stop "Error write_kinetic_value: Nlso != Nlat*Nso" 
+       !
+       unit = free_unit()
+       open(unit,file="dmft_kinetic_energy.info")
+       write(unit,"(A1,90(A14,1X))")"#",&
+            str(1)//"<K>",str(2)//"<Eloc>",&
+            (str(2+i)//"<K"//str(i)//">",i=1,Nso),&
+            (str(2+Nso+i)//"<Eloc"//str(i)//">",i=1,Nso)
+       close(unit)
+       !
+       !
+       allocate(Ekin_(Nlat,Nso))
+       allocate(Eloc_(Nlat,Nso))
+       do ilat=1,Nlat
+          do iso=1,Nso
+             i = iso + (ilat-1)*Nso
+             Ekin_(ilat,iso) = Ekin(i)
+             Eloc_(ilat,iso) = Eloc(i)
+          enddo
+       enddo
+       !
+       unit = free_unit()
+       open(unit,file="dmft_kinetic_energy.dat")       
+       do ilat=1,Nlat
+          write(unit,"(100000F15.9)")sum(Ekin_(ilat,:)),sum(Eloc_(ilat,:)),&
+               (Ekin_(ilat,i),i=1,Nso),&
+               (Eloc_(ilat,i),i=1,Nso)
+       enddo
+       write(unit,"(90F15.9)")sum(Ekin_)/Nlat,sum(Eloc_)/Nlat
+       close(unit)
+    endif
+  end subroutine write_kinetic_value
 
 
 end program lancED
